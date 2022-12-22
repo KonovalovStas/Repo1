f416cd097bdaa972ac83664175116996f4e02d66
bobygoon <bobygoon7@gmail.com>
2022-12-22 20:10:11 +0400

    add005

--- Ex005_ifElse/Program.cs
+++ Ex005_ifElse/Program.cs
@@ -1,12 +1,18 @@
 ﻿Console.Write("Введите имя пользователя: ");
-string username = Console.ReadLine;
+string username = Console.ReadLine();
 
-if (username == "Маша") ;
+if (username.ToLower() == "маша")
 {
-    Console.WriteLine("Ура это же Маша! ");
+    Console.WriteLine("Это же Маша!");
 }
 else
 {
     Console.Write("Привет, ");
     Console.WriteLine(username);
 }
+
+
+
+
+
+
