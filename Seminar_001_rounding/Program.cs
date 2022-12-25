56ae055e3cc58ff66f2a0a1893d503a6b6592f1c
bobygoon <bobygoon7@gmail.com>
2022-12-25 13:21:41 +0400

    Add округление и файл Readme

---
+++ Seminar_001_rounding/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("------------------\n");
+double number = 7, numberA = 8.5;
+
+double answer = number / numberA;
+
+answer += 6;
+answer -= 1;
+answer *= 4;
+answer /= 2;
+
+Console.Write(Math.Round(answer, 3));
