9081a2aa3dbc629ff2b596984eacb4f0453d748e
bobygoon <bobygoon7@gmail.com>
2022-12-25 11:57:20 +0400

    Calculator

---
+++ Seminar_001_Calculator/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("------------------\n");
+double number = 15;
+double numberA = 85;
+
+Console.Write($"{number} * {numberA} = {number * numberA}\n ");
+Console.Write($"{number} / {numberA} = {number / numberA}\n ");
+Console.Write($"{number} + {numberA} = {number + numberA}\n ");
+Console.Write($"{number} - {numberA} = {number - numberA}\n ");
