79bddb4f7034fbb2922769b554a7cc0c5938a82d
bobygoon <bobygoon7@gmail.com>
2022-12-25 11:44:19 +0400

    сложение, деление, умножение

---
+++ Seminar_001/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("------------------\n");
+int number = 7;
+int numberA = 8;
+int numberB = 10;
+
+Console.Write($"Сумма переменных = {number + numberA + numberB}\n");
+
+Double numberC = 100;
+Double numberD = 20;
+
+Console.Write($"Итог по делению = {numberC / numberD}\n");
+
+Console.Write($"Итоговый результат = {(number + numberA + numberB) * (numberC / numberD)}\n ");
