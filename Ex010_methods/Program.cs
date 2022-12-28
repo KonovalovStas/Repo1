031f5ab4a29f7ddadf0f9ce870b72f546831692c
bobygoon <bobygoon7@gmail.com>
2022-12-28 21:23:33 +0400

    Add methods

---
+++ Ex010_methods/Program.cs
@@ -0,0 +1,21 @@
+﻿int[] array = { 1, 2, 3, 4, 65, 4, 3, 66, 78 };
+
+int n = array.Length;
+
+int find = 4;
+
+int index = 0;
+
+while (index < n)
+{
+
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    //index = index +1;
+    index++;
+}
+
+
