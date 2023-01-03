f20c03b6050ee05f9ae71f7c53d644ecd81b0546
bobygoon <bobygoon7@gmail.com>
2023-01-03 14:15:03 +0400

    изучили функции

---
+++ Seminar_002_5_Task_functions/Program.cs
@@ -0,0 +1,20 @@
+﻿string name;
+
+Console.Write($"Hello, what's your name? : ");
+name = Console.ReadLine()!;
+string name2 = "Gulnaz";
+string name3 = "Arishka";
+
+PrintText(name);
+PrintText(name2);
+PrintText(name3);
+
+void PrintText(string text)
+{
+    for (int i = 0; i < text.Length + 2; i++)
+        Console.Write("-");
+    Console.Write($"\n|{text}|\n");
+    for (int i = 0; i < text.Length + 2; i++)
+        Console.Write("-");
+    Console.WriteLine();
+}
