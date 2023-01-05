eb86395c7a6744dc40f032d8b65ebd627157f3e4
bobygoon <bobygoon7@gmail.com>
2023-01-05 21:06:12 +0400

    for

---
+++ Ex012for/Program.cs
@@ -0,0 +1,13 @@
+﻿string Method4(int count, string text)
+{
+
+    string result = string.Empty;
+    for (int i = 0; i < count; i++)
+    {
+        result = result + text;
+    }
+    return result;
+}
+
+string res = Method4(10, "asdf\n");
+Console.WriteLine(res);
