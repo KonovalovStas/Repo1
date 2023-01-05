ac864d7e7ee4439ab8a90d73669837b74bfd5c75
bobygoon <bobygoon7@gmail.com>
2023-01-05 21:06:36 +0400

    task

---
+++ Ex012Task/Program.cs
@@ -0,0 +1,33 @@
+﻿int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1 };
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.WriteLine($"{array[i]}");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+
+        for (int j = i + 1; i < array.Length; i++)
+        {
+            if (array[j] < array[minPosition]) minPosition = j;
+        }
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+
+PrintArray(arr);
+SelectionSort(arr);
+
+PrintArray(arr);
